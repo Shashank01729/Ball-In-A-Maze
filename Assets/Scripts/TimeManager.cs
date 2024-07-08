2faaefe3d1997539561c6d9db298e69454eaba08
Shashank01729 <117083031+Shashank01729@users.noreply.github.com>
2024-07-08 21:16:16 +0530

    WIN LOSE

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -5,10 +5,12 @@ using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour
 {
+    public CollectableManager collectableManager;
     public float timeValue = 60f;
     private bool isTimerRunning = false;
     private float currentTime;
     public Text timerText;
+    // public Text gameMsg;
 
     private void Start()
     {
@@ -20,10 +22,21 @@ public class TimeManager : MonoBehaviour
     {
         if (isTimerRunning) {
             currentTime -= Time.deltaTime;
-            if (currentTime <= 0.0f) { 
+            if (currentTime <= 0.0f && !collectableManager.allCoinsCollected) { 
                 currentTime = 0.0f;
                 isTimerRunning=false;
+                Debug.Log("Game Lost");
+                // gameMsg.text = "  GAME LOST";
+                GameManager.instance.GameOverScreen(false);
             }
+
+            if (collectableManager.allCoinsCollected) {
+                Debug.Log("Game Won");
+                isTimerRunning = false;
+                // gameMsg.text = "  GAME WON";
+                GameManager.instance.GameOverScreen(true);
+            }
+
             UpdateTimerText();
         }
     }
